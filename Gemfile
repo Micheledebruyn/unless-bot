e82c6037c3f591d77a23701306ddda19ddabeae9
Lars Bohm <larsbohm@gmail.com>
2017-03-13 17:47:40 +0100

    setup production database rule

--- Gemfile
+++ Gemfile
@@ -5,5 +5,12 @@ gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'sinatra-activerecord'
 gem 'rake'
-gem 'sqlite3'
 gem 'facebook-messenger'
+
+group :development do 
+  gem 'sqlite3'
+end
+
+group :production do 
+  gem 'pg'
+end
--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,3 +1,6 @@
+require 'csv'
+require 'open-uri'
+require 'json'
 require 'facebook/messenger'
 include Facebook::Messenger
 # NOTE: ENV variables should be set directly in terminal for testing on localhost
@@ -6,9 +9,34 @@ include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
-  if message.text == "hello"
-    message.reply(text: 'Yoyo, whaddup')
-  else
-    message.reply(text: 'Hello!')
+  # if message.text == "hello"
+  #   message.reply(text: 'Yoyo, whaddup')
+  # else
+  #   message.reply(text: 'Hello!')
+  # end
+  
+
+  message.reply(
+    attachment: {
+      type: 'image',
+      payload: {
+        url: Api.new.searcher(message.text)
+      }
+    }
+    )
+end
+
+
+class Api
+  def searcher(tag)
+    api_url = "http://api.giphy.com/v1/gifs/search?q=#{tag}&api_key=dc6zaTOxFJmzC"
+    open(api_url) do |stream|
+      response = JSON.parse(stream.read)
+      if response["data"] != []
+        result = response["data"].sample["images"]["fixed_height"]["url"]
+      else
+        result = 'http://media1.giphy.com/media/F9AU77Krzw8ta/200.gif'
+      end
+    end
   end
 end
